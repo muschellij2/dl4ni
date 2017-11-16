79da6f5c9bac08eaaafd51bcc1a45dab0e6f4d8a
Domingo Lopez <dominlopez78@gmail.com>
2017-11-16 11:12:11 +0100

    Little modification of parse_optimizer

--- R/parse_optimizer.R
+++ R/parse_optimizer.R
@@ -1,4 +1,15 @@
 parse_optimizer <- function(optimizer) {
   
+  if (is.character(optimizer)) {
+    
+    # Parse the string
+    
+  } else {
+    
+    
+    
+  }
+  
+  return(list(name = name, params = params))
   
 }
