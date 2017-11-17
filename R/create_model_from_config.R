5c0522310fb639026a9c3062fce49e41c6688e41
Domingo Lopez <dominlopez78@gmail.com>
2017-11-17 13:33:49 +0100

    Added documentation for create_model_from_config and update the function to build autoencoders

--- R/create_model_from_config.R
+++ R/create_model_from_config.R
@@ -1,12 +1,12 @@
-#' @title FUNCTION_TITLE
+#' @title Builds a DLmodel from a Given Configuration
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function uses a previously defined configuration (\code{DLconfig} object) to create and compile
+#' a keras model, stored, with the rest of metadata, in a \code{DLmodel} object.
 #'
-#' @param config    (name) PARAM_DESCRIPTION
+#' @param config    (\code{DLconfig} object) The configuration to use to build a model.
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return The \code{DLmodel} object created according to the given configuration.
 #'
-#' @details DETAILS
 #' @export 
 #' 
 create_model_from_config <- function(config) {
@@ -16,25 +16,30 @@ create_model_from_config <- function(config) {
   # Basic check
   stopifnot(inherits(config, "DLconfig"))
   
-  result <- list()
-  
+  # Add layers corresponding to the features input.
   input_features <- layer_input(shape = c(config$num_features))
   
   output_features <- input_features %>% 
     add_layers(layers_definition = config$feature_layers,
                clf = FALSE)
   
+  # Check number of input images
   num_vol_inputs <- length(config$vol_layers)
   
+  # Initialize inputs and outputs of the "volumes" paths.
   vol_inputs <- list()
   vol_outputs <- list()
   
+  # For each input image
   for (v_input in seq(num_vol_inputs)) {
     
+    # The input is always a vector, with width ^ 3 components for each of the volumes inside the image
+    # (there can be 4D images).
     vol_inputs[[v_input]] <- layer_input(shape = c(config$num_volumes[v_input] * config$width ^ 3)) 
     
     vol_outputs[[v_input]] <- vol_inputs[[v_input]]
     
+    # When using only convolutional layers, we have to reshape the input to match the volume
     if (config$only_convolutionals) {
       
       vol_outputs[[v_input]] <- (vol_outputs[[v_input]]) %>% 
@@ -43,12 +48,17 @@ create_model_from_config <- function(config) {
       
     }
     
+    # Add layers in this path
     vol_outputs[[v_input]] <- (vol_outputs[[v_input]]) %>% 
       add_layers(layers_definition = config$vol_layers[[v_input]],
                  clf = FALSE) 
     
+    # Shape of the output of the path
     shape <- vol_outputs[[v_input]] %>% object_shape()
     
+    # If we are not considering convolutional layers, and the output of the path is volumetric 
+    # (length(shape) > 2), then flatten the output, since in succesive layers we can combine
+    # this output with dense layers.
     if (!config$only_convolutionals & length(shape) > 2) {
       
       vol_outputs[[v_input]] <- (vol_outputs[[v_input]]) %>%
@@ -56,6 +66,7 @@ create_model_from_config <- function(config) {
       
     }
     
+    # Concatenate the individual volume outputs
     if (v_input == 1) {
       
       individual_outputs <- vol_outputs[[v_input]]
@@ -68,6 +79,7 @@ create_model_from_config <- function(config) {
     
   }
   
+  # According to the path definition, we keep only the features path, the volumes paths, or both, concatenated.
   output_vol <- individual_outputs
   
   main_output <- switch(config$path[1],
@@ -75,7 +87,7 @@ create_model_from_config <- function(config) {
                         "features" = output_features,
                         "volumes"  = output_vol)
   
-  
+  # Add layers in the common part of the model
   if (length(config$common_layers) > 0) {
     
     main_output <- main_output %>% 
@@ -87,6 +99,7 @@ create_model_from_config <- function(config) {
   # Finalize with convolutional?
   if (config$finalize_with_convolutional) {
     
+    # Add a new convolutional layer, forcing its output to have the desired dimensions
     layer_to_add <- list(conv3d(filters = 1, 
                                 kernel_size = rep(config$convolutional_kernel_size, 3),
                                 force = config$output_width,
@@ -101,11 +114,14 @@ create_model_from_config <- function(config) {
   # Check that last-but-one layer in case of "only_convolutionals" has the appropriate shape
   if (config$only_convolutionals) {
     
+    # Object and expected shapes.
     shape <- main_output %>% object_shape()
     expected_shape <- c(config$output_width, config$output_width, config$output_width)
     
+    # When using deconvolutional layers for upsampling, the output is all NAs. This is allowed.
     if (!all(is.na(shape[-4]))) {
       
+      # Otherwise, if output shape and expected shapes are not consistent, raise an error.
       if (!all(shape[-4] == expected_shape))
         stop(paste0("Output shapes are not correct. Expected: (", 
                     paste0(expected_shape, collapse = ", "),
@@ -126,54 +142,72 @@ create_model_from_config <- function(config) {
     
   }
   
+  # Define the inputs, according to the config$path.
+  inputs <- switch(config$path[1],
+         
+         "volumes" = vol_inputs,
+         
+         "both" = c(input_features, vol_inputs),
+         
+         "features" = input_features)
+  
+  # If we have decoder layers, define the encoder, decoder and complete model.
   if (!is.null(config$decoder_layers)) {
     
-    encoder <- keras_model(inputs = list(input_features, vol_inputs),
+    # Encoder from the inputs to the current output.
+    encoder <- keras_model(inputs = inputs,
                            outputs = main_output)
     
-    decoder_input <- layer_input(shape = c(config$last_layer$params$units))
+    n_layers_encoder <- length(encoder$layers)
     
-    decoder_output <- decoder_input %>%
+    # Build full model
+    full_output <- main_output
+    
+    full_output <- full_output %>% 
       add_layers(layers_definition = config$decoder_layers,
                  clf = FALSE)
     
+    model <- keras_model(inputs = inputs, outputs = full_output)
+    n_layers_full <- length(model$layers)
+    
+    # Decoder begins with a layer of the same shape as the output of the encoder.
+    decoder_input <- layer_input(shape = main_output %>% object_shape())
+    
+    # Add the SAME decoder layers
+    decoder_output <- decoder_input
+    for (layer in seq(n_layers_encoder + 1, n_layers_full)) {
+      
+      decoder_output <- model$layers[[layer]](decoder_output)
+      
+    }
+    
+    # And create the decoder model
     decoder <- keras_model(inputs = decoder_input,
                            outputs = decoder_output)
     
-    main_output <- main_output %>% 
-      add_layers(layers_definition = config$decoder_layers,
-                 clf = FALSE)
-    
   } else {
     
+    # No autoncoder
+    # Create the model
+    model <- keras_model(inputs = inputs, outputs = main_output)
     encoder <- NULL
     decoder <- NULL
     
-    
   }
   
-  model <- switch(config$path[1],
-                  
-                  "volumes" = keras_model(inputs = vol_inputs,
-                                          outputs = main_output),
-                  
-                  "both" = keras_model(inputs = c(input_features, vol_inputs),
-                                       outputs = main_output),
-                  
-                  "features" = keras_model(inputs = input_features,
-                                           outputs = main_output) )
   
-  
+  # Compile if we are given the optimizer and loss function
   if (!is.null(config$optimizer) && !is.null(config$loss)) {
     
     # Get the actual optimizer to use, according to the stored configuration
     optimizer <- eval_optimizer(config$optimizer)
     
-    # And compile
+    # And compile. There is no need to compile encoder and decoder since they are not to be trained.
     model %>% compile(optimizer = optimizer, loss = config$loss)
     
   }
   
+  # Store the model and metadata in an object of class DLmodel
   result <- new.env()
   result$model <- model
   result$width <- config$width
