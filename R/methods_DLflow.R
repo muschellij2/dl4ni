57448248913c260cc47969ae75bb314721df9eb0
Domingo Lopez <dominlopez78@gmail.com>
2017-11-27 11:10:47 +0100

    Added documentation to DLflow.

--- R/methods_DLflow.R
+++ R/methods_DLflow.R
@@ -1,231 +1,208 @@
-#' @title FUNCTION_TITLE
+#' @title Flow Creation
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function creates a new \code{DLflow}, with given name and inputs.
 #'
-#' @param name      (character) PARAM_DESCRIPTION, Default: ''
-#' @param inputs    (call) PARAM_DESCRIPTION, Default: list()
+#' @param name      (character) Name of the flow, Default: ''
+#' @param inputs    (list) Character vector or list with the names of the inputs, Default: list()
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return A new flow.
 #'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.initialize
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Name of a Flow
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function returns the name of a flow.
 #'
+#' @return The name of the flow as a string.
 #'
-#' @return OUTPUT_DESCRIPTION
-#'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.name
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Flow Inputs
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function returns the list of names of the inputs to this flow.
 #'
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return A character vector with the names of the inputs of the flow.
 #'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.get_inputs
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Flow Outputs
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function returns the list of names of the outputs available in this flow.
 #'
+#' @return A character vector with the names of the available outputs.
 #'
-#' @return OUTPUT_DESCRIPTION
-#'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.get_outputs
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Models from a Flow
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function returns a model that computes a specific output in a flow.
 #'
-#' @param output    (name) PARAM_DESCRIPTION
+#' @param output    (character) The name of the output that the required model computes.
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return A \code{\link{DLmodel}} that is used to compute the given output.
 #'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.get_model
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Replace Part of a Flow
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function is used to replace a part of the flow, such as a \code{\link{DLmodel}}, a
+#' \code{\link{DLscheme}} or a function, with another object of one these classes.
 #'
-#' @param output    (name) PARAM_DESCRIPTION
-#' @param with      (name) PARAM_DESCRIPTION
+#' @param output    (character) Name of the output (part of the flow) to substitute.
+#' @param with      (\code{\link{DLmodel}}, \code{\link{DLscheme}} or function) The object that is replacing the previous one.
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return The flow with the replaced object.
 #'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.replace
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Add an Item to a Flow
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function adds an item (input, \code{\link{DLscheme}}, \code{\link{DLmodel}} or function) to the flow.
 #'
-#' @param what      (NULL) PARAM_DESCRIPTION, Default: NULL
-#' @param inputs    (NULL) PARAM_DESCRIPTION, Default: NULL
-#' @param output    (NULL) PARAM_DESCRIPTION, Default: NULL
-#' @param subset    (NULL) PARAM_DESCRIPTION, Default: NULL
+#' @param what      (character, \code{\link{DLscheme}}, \code{\link{DLmodel}} or function) Item to add, Default: NULL
+#' @param inputs    (character) List of required inputs, Default: NULL
+#' @param output    (character) The "name " of the item inside the flow, Default: NULL
+#' @param subset    (list) PARAM_DESCRIPTION, Default: NULL
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return The updated flow.
 #'
-#' @details DETAILS
+#' @details If the item to add is an input, the only required (and used) argument is \code{what}. If it's a function or a \code{\link{DLscheme}} or a \code{\link{DLmodel}}, mandatory arguments are \code{what}, \code{inputs} and \code{output}.
+#' 
 #' @export 
 #' @name DLflow.add
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Execute a Flow
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function runs the computation graph of the flow to obtain some outputs, given input files.
 #'
-#' @param inputs                (call) PARAM_DESCRIPTION, Default: list()
-#' @param desired_outputs       (NULL) PARAM_DESCRIPTION, Default: NULL
-#' @param initialize_outputs    (logical) PARAM_DESCRIPTION, Default: TRUE
-#' @param mode                  (call) PARAM_DESCRIPTION, Default: c("debug", "faster", "medium", "slower")
+#' @param inputs                (list) List of input filenames, Default: list()
+#' @param desired_outputs       (character vector) List of names of the outputs to compute, Default: NULL
+#' @param initialize_outputs    (logical) Delete previous runs?, Default: TRUE
+#' @param mode                  (character) Inference mode, Default: c("debug", "faster", "medium", "slower")
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return A list with as many (named) fields as desired outputs. The names of the fields are those of the outputs.
 #'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.execute
 NULL
 
-#' @title FUNCTION_TITLE
-#'
-#' @description FUNCTION_DESCRIPTION
-#'
-#' @param ...    (name) PARAM_DESCRIPTION
-#'
-#' @return OUTPUT_DESCRIPTION
-#'
-#' @details DETAILS
+#' @title Execute a Flow
+#' @description This function runs the computation graph of the flow to obtain some outputs, given input files.
+#' @describeIn DLflow.execute
 #' @export 
 #' @name DLflow.run
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Train Part of a Flow
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function is used to train a \code{\link{DLmodel}} that specifies an output of the flow.
 #'
-#' @param output                     (name) PARAM_DESCRIPTION
-#' @param input_filenames            (name) PARAM_DESCRIPTION
-#' @param output_filenames           (name) PARAM_DESCRIPTION
-#' @param train_split                (numeric) PARAM_DESCRIPTION, Default: 0.75
-#' @param epochs                     (numeric) PARAM_DESCRIPTION, Default: 10
-#' @param target_windows_per_file    (numeric) PARAM_DESCRIPTION, Default: 1024
-#' @param mode                       (call) PARAM_DESCRIPTION, Default: c("debug", "faster", "medium", "slower")
+#' @param output                     (charcater) Name of the output (model) to train.
+#' @param input_filenames            (list) List of input files to train with, that is, the X in the training phase.
+#' @param output_filenames           (list) List of output files, representing the Y in the training phase.
+#' @param train_split                (numeric) Ratio of input files used for training, the remaining are used for validation, Default: 0.75
+#' @param epochs                     (numeric) Number of epochs to train for, Default: 10
+#' @param target_windows_per_file    (numeric) Number of windows to extract, as a minimum, per file, Default: 1024
+#' @param mode                       (character) Inference mode for the required inputs of this part of the flow, in case they had to be computed, Default: c("debug", "faster", "medium", "slower")
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return The flow with the trained model.
 #'
-#' @details DETAILS
+#' @details In case the part of the flow is currently defined by a \code{\link{DLscheme}}, a \code{\link{DLmodel}} is instatiated
+#' taking into account given inputs and output filenames. This is the model to be trained.
+#' 
 #' @export 
 #' @name DLflow.train
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Flow Graph
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function returns the computation graph of the flow.
 #'
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return The graph, as an \code{\link{igraph}} object.
 #'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.graph
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Plot a Flow
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function plots the computation graph of the flow.
 #'
-#' @param interactive    (logical) PARAM_DESCRIPTION, Default: FALSE
+#' @param interactive    (logical) Use interactive JavaScript visualization?, Default: FALSE
 #'
-#' @return OUTPUT_DESCRIPTION
-#'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.plot
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Reset Outputs of a Flow
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function deletes previous computed results in a flow.
 #'
-#' @param outputs    (character) PARAM_DESCRIPTION, Default: 'all'
+#' @param outputs    (character) List of computed outputs to remove, Default: 'all'
 #'
-#' @return OUTPUT_DESCRIPTION
-#'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.reset
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Save a Flow
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function saves a flow to disk.
 #'
-#' @param path           (call) PARAM_DESCRIPTION, Default: tempdir()
-#' @param file_prefix    (call) PARAM_DESCRIPTION, Default: self$name()
+#' @param path           (character) Path where to store the flow, Default: tempdir()
+#' @param file_prefix    (character) Filename, Default: the flow name.
 #'
-#' @return OUTPUT_DESCRIPTION
-#'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.save
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Load a Flow
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function loads a flow from disk.
 #'
-#' @param filename    (name) PARAM_DESCRIPTION
+#' @param filename    (character) Path to a file where a flow is stored.
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return The loaded flow.
 #'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.load
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Flow Subsetting
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function extracts just a subflow from the parent flow, specifying the outputs to retain.
 #'
-#' @param outputs    (name) PARAM_DESCRIPTION
+#' @param outputs    (character vector) List of the outputs to retain.
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return A new flow which is complete in the sense that it contains the required \code{outputs} and all parts of the bigger flow
+#' which lead to those outputs.
 #'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.subset
 NULL
 
-#' @title FUNCTION_TITLE
+#' @title Clone Flow
 #'
-#' @description FUNCTION_DESCRIPTION
+#' @description This function clones a flow.
 #'
-#' @param deep    (logical) PARAM_DESCRIPTION, Default: FALSE
+#' @param deep    (logical) Deep copy of the flow?, Default: FALSE
 #'
-#' @return OUTPUT_DESCRIPTION
+#' @return A new flow with exactly the same information.
 #'
-#' @details DETAILS
 #' @export 
 #' @name DLflow.clone
 NULL
