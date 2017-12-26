1f3450ccca62aa3af8ce6ded2a43cc314cf1a8dd
Domingo Lopez <dominlopez78@gmail.com>
2017-12-26 13:55:52 +0100

    Added some # nocov tags

--- R/get_problem_info.R
+++ R/get_problem_info.R
@@ -55,12 +55,16 @@ get_problem_info <- function(problem_path = NULL,
     
     if (length(input_dirs) > 1 & interactive) {
       
+      # nocov start
+      
       choices <- sapply(input_dirs, basename)
       title <- "Select inputs to use:"
       chosen_inputs <- select.list(choices = c(choices, "all"), multiple = TRUE, title = title)
       if ("all" %in% chosen_inputs) chosen_inputs <- choices
       chosen_inputs <- match(chosen_inputs, choices)
       num_inputs <- length(chosen_inputs)
+      
+      # nocov end
       
     } else {
       
@@ -125,12 +129,16 @@ get_problem_info <- function(problem_path = NULL,
       # Select one of them, if we are in an interactive session
       if (interactive) {
         
+        # nocov start
+        
         choices <- sapply(output_dirs, basename)
         title <- "Choose a possible output:"
         chosen_output <- utils::select.list(choices = choices, title = title)
         chosen_output <- match(chosen_output, choices)
         output_path <- output_dirs[chosen_output]
-        
+
+        # nocov end        
+      
       } else {
         
         # If in a non-interactive session, take the first one
