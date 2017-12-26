f0158e64a4f127ee94e9097af3a58bd30d456177
Domingo Lopez <dominlopez78@gmail.com>
2017-12-26 14:09:01 +0100

    Added more tests

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -3,5 +3,13 @@ context("utils")
 
 test_that("utils works as expected", {
 
-
+  
+  expect_equal(convert_to_bytes("1G"), 1024 * 1024 * 1024)
+  
+  expect_equal(round_to_multiple(100), 32 * 3)
+  expect_equal(round_to_multiple(100, upper = TRUE), 32 * 4)
+  
+  expect_equal(round_to_power2(10), 8)
+  expect_equal(round_to_power2(100), 64)
+  
 })
